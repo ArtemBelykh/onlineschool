6040359047c826e1f0bf6a75b3501044a09ca311
StellGG <stell_artem@mail.ru>
2023-04-03 01:22:22 +0300

    Last Update Off Site

--- src/Components/StartMain.tsx
+++ src/Components/StartMain.tsx
@@ -5,19 +5,23 @@ import ThirdScreen from "./Screen/ThirdScreen";
 import FourthScreen from "./Screen/FourthScreen";
 import FifthScreen from "./Screen/FifthScreen";
 import SixthScreen from "./Screen/SixthScreen";
+import Typography from "@mui/material/Typography";
 
 const StartMain = () => {
     return (
         <div>
 
-            <FirstScreen/>
-            <SecondScreen/>
+            <Typography variant="h1" component="h1">Сайт не работает!</Typography>
 
-            <ThirdScreen/>
-            <FourthScreen/>
-            <FifthScreen/>
 
-            <SixthScreen/>
+            {/*<FirstScreen/>*/}
+            {/*<SecondScreen/>*/}
+
+            {/*<ThirdScreen/>*/}
+            {/*<FourthScreen/>*/}
+            {/*<FifthScreen/>*/}
+
+            {/*<SixthScreen/>*/}
 
         </div>
     );
