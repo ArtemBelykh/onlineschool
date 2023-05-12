b7860cd55f67ef39c5622c6b92bfb63277cee005
StellGG <stell_artem@mail.ru>
2023-05-12 17:33:04 +0300

    first commit

--- src/Components/ModalMain.tsx
+++ src/Components/ModalMain.tsx
@@ -99,7 +99,7 @@ const ModalMain = ({styleBtn, variantBtn, colorBtn, titleBtn}: IModalMain) => {
                                        variant="standard"/>
 
                             <InputLabel sx={{
-                                marginLeft: "-40px!important",
+                                marginLeft: "-20px!important",
                                 marginTop: "5px!important",
                                 marginBottom: "0px!important"
                             }}>
