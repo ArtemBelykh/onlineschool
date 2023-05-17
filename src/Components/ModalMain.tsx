ca29f90bd0c950773fa6af4ab04c325dde8cf63e
StellGG <stell_artem@mail.ru>
2023-05-17 22:30:19 +0300

    17.05.23

--- src/Components/ModalMain.tsx
+++ src/Components/ModalMain.tsx
@@ -10,11 +10,6 @@ import Select, {SelectChangeEvent} from '@mui/material/Select';
 import CloseIcon from '@mui/icons-material/Close';
 import {useForm} from 'react-hook-form';
 import {MuiTelInput} from "mui-tel-input";
-import {DemoContainer} from '@mui/x-date-pickers/internals/demo';
-import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
-import {DateField} from '@mui/x-date-pickers/DateField';
-import {DatePicker} from "@mui/x-date-pickers";
 
 
 export interface IModalMain {
@@ -35,21 +30,6 @@ const style = {
     p: 2,
     borderRadius: "10px"
 };
-
-const dateNow = new Date(); // Creating a new date object with the current date and time
-const year = dateNow.getFullYear(); // Getting current year from the created Date object
-const monthWithOffset = dateNow.getUTCMonth() + 1; // January is 0 by default in JS. Offsetting +1 to fix date for calendar.
-const month = // Setting current Month number from current Date object
-    monthWithOffset.toString().length < 2 // Checking if month is < 10 and pre-prending 0 to adjust for date input.
-        ? `0${monthWithOffset}`
-        : monthWithOffset;
-const date =
-    dateNow.getUTCDate().toString().length < 2 // Checking if date is < 10 and pre-prending 0 if not to adjust for date input.
-        ? `0${dateNow.getUTCDate()}`
-        : dateNow.getUTCDate();
-
-const materialDateInput = `${year}-${month}-${date}`; // combining to format for defaultValue or value attribute of material <TextField>
-
 
 const ModalMain = ({styleBtn, variantBtn, colorBtn, titleBtn}: IModalMain) => {
     const [value, setValue] = React.useState('')
