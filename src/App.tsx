1282a948080f2b160202b113b2b9e6a5870250c0
StellGG <stell_artem@mail.ru>
2023-05-17 17:24:52 +0300

    17.05.23

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,44 @@
-import React from 'react';
+import React, {Suspense} from 'react'
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import NotFoundPages from "./Components/Pages/NotFound.pages";
-import HowToChooseADrivingSchoolForABeginnerArticle
-    from "./Components/Pages/Articles/HowToChooseADrivingSchoolForABeginner.article";
-import HowMeArticle
-    from "./Components/Pages/Articles/HowMe.article";
-import HowToSaveMoneyArticle from "./Components/Pages/Articles/HowToSaveMoney.article";
-import MedicalTestOnTheRoad from "./Components/Pages/Articles/MedicalTestOnTheRoad.article";
-import HonestHoursInDrivingSchool from "./Components/Pages/Articles/HonestHoursInDrivingSchool.article";
-import GetOutOfTheCarOrNotArticle from "./Components/Pages/Articles/GetOutOfTheCarOrNot.article";
-import HowToBuyAUsedCar from "./Components/Pages/Articles/HowToBuyAUsedCar.article";
-import WuCategoriesArticle from "./Components/Pages/Articles/WuCategories.article";
-import EuroProtocolArticle from "./Components/Pages/Articles/EuroProtocol.article";
-import StartMain from "./Components/StartMain";
-import LicensePages from "./Components/Pages/License.pages";
+const NotFoundPages = React.lazy(() => import("./Components/Pages/NotFound.pages"))
+const HowToChooseADrivingSchoolForABeginnerArticle = React.lazy(() => import("./Components/Pages/Articles/HowToChooseADrivingSchoolForABeginner.article"))
+const HowMeArticle = React.lazy(() => import("./Components/Pages/Articles/HowMe.article"))
+const HowToSaveMoneyArticle = React.lazy(() => import("./Components/Pages/Articles/HowToSaveMoney.article"))
+const MedicalTestOnTheRoad = React.lazy(() => import("./Components/Pages/Articles/MedicalTestOnTheRoad.article"))
+const HonestHoursInDrivingSchool = React.lazy(() => import("./Components/Pages/Articles/HonestHoursInDrivingSchool.article"))
+const GetOutOfTheCarOrNotArticle = React.lazy(() => import("./Components/Pages/Articles/GetOutOfTheCarOrNot.article"))
+const HowToBuyAUsedCar = React.lazy(() => import("./Components/Pages/Articles/HowToBuyAUsedCar.article"))
+const WuCategoriesArticle = React.lazy(() => import("./Components/Pages/Articles/WuCategories.article"))
+const EuroProtocolArticle = React.lazy(() => import("./Components/Pages/Articles/EuroProtocol.article"))
+const StartMain = React.lazy(() => import("./Components/StartMain"))
+const LicensePages = React.lazy(() => import("./Components/Pages/License.pages"))
 
 function App() {
     return (
         <>
             <Navbar/>
-            <Routes>
-                <Route path="/" element={<StartMain/>}/>
-                <Route path="/license" element={<LicensePages/>}/>
-                <Route path="*" element={<NotFoundPages/>}/>
 
-                <Route path="/article/How-to-save-money" element={<HowToSaveMoneyArticle/>}/>
-                <Route path="/article/How-To-Choose-a-driving-school-for-a-beginner"
-                       element={<HowToChooseADrivingSchoolForABeginnerArticle/>}/>
-                <Route path="/article/How-Me" element={<HowMeArticle/>}/>
-                <Route path="/article/Medical-Test-On-The-Road" element={<MedicalTestOnTheRoad/>}/>
-                <Route path="/article/Honest-Hours-In-Driving-School" element={<HonestHoursInDrivingSchool/>}/>
-                <Route path="/article/Get-a-car" element={<GetOutOfTheCarOrNotArticle/>}/>
-                <Route path="/article/Buy-Car" element={<HowToBuyAUsedCar/>}/>
-                <Route path="/article/Wu-category" element={<WuCategoriesArticle/>}/>
-                <Route path="/article/Euro-Protocol" element={<EuroProtocolArticle/>}/>
-            </Routes>
+            <Suspense fallback="load">
+                <Routes>
+                    <Route path="/" element={<StartMain/>}/>
+                    <Route path="/license" element={<LicensePages/>}/>
+                    <Route path="*" element={<NotFoundPages/>}/>
+
+                    <Route path="/article/How-to-save-money" element={<HowToSaveMoneyArticle/>}/>
+                    <Route path="/article/How-To-Choose-a-driving-school-for-a-beginner"
+                           element={<HowToChooseADrivingSchoolForABeginnerArticle/>}/>
+                    <Route path="/article/How-Me" element={<HowMeArticle/>}/>
+                    <Route path="/article/Medical-Test-On-The-Road" element={<MedicalTestOnTheRoad/>}/>
+                    <Route path="/article/Honest-Hours-In-Driving-School" element={<HonestHoursInDrivingSchool/>}/>
+                    <Route path="/article/Get-a-car" element={<GetOutOfTheCarOrNotArticle/>}/>
+                    <Route path="/article/Buy-Car" element={<HowToBuyAUsedCar/>}/>
+                    <Route path="/article/Wu-category" element={<WuCategoriesArticle/>}/>
+                    <Route path="/article/Euro-Protocol" element={<EuroProtocolArticle/>}/>
+                </Routes>
+            </Suspense>
 
             <Footer/>
 
