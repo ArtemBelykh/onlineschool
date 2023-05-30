9acfb3271ffb5bef1df36e293b4ab2f8b47f0802
StellGG <stell_artem@mail.ru>
2023-05-31 00:31:52 +0300

    30.05.23

--- src/Components/Screen/SixthScreen.tsx
+++ src/Components/Screen/SixthScreen.tsx
@@ -29,19 +29,19 @@ const SixthScreen = () => {
             }}>
 
                 <div style={{position: "relative", overflow: "hidden"}} className="adaptive__maps_div2">
-                    {/*<a*/}
-                    {/*    href="https://yandex.ru/maps/7/kostroma/?utm_medium=mapframe&utm_source=maps"*/}
-                    {/*    style={{color: "#eee", fontSize: "12px", position: "absolute", top: "0px"}}>Кострома</a>*/}
-                    {/*<a*/}
-                    {/*    href="https://yandex.ru/maps/7/kostroma/house/sovetskaya_ulitsa_44a/YEkYfwNlT0cDQFttfXt3c39rbA==/?ll=40.945624%2C57.762388&utm_medium=mapframe&utm_source=maps&z=17.1"*/}
-                    {/*    style={{color: "#eee", fontSize: "12px", position: "absolute", top: "14px"}}>Советская улица,*/}
-                    {/*    44А —*/}
-                    {/*    Яндекс*/}
-                    {/*    Карты</a>*/}
-                    {/*<iframe title={"ya_maps"} className="adaptive__maps"*/}
-                    {/*        src="https://yandex.ru/map-widget/v1/?ll=40.945624%2C57.762388&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjM0ODY2NRJD0KDQvtGB0YHQuNGPLCDQmtC-0YHRgtGA0L7QvNCwLCDQodC-0LLQtdGC0YHQutCw0Y8g0YPQu9C40YbQsCwgNDTQkCIKDVHII0IVrwxnQg%2C%2C&z=17.1"*/}
-                    {/*        width="700" height="476" allowFullScreen={true}*/}
-                    {/*        style={{position: "relative"}}></iframe>*/}
+                    <a
+                        href="https://yandex.ru/maps/7/kostroma/?utm_medium=mapframe&utm_source=maps"
+                        style={{color: "#eee", fontSize: "12px", position: "absolute", top: "0px"}}>Кострома</a>
+                    <a
+                        href="https://yandex.ru/maps/7/kostroma/house/sovetskaya_ulitsa_44a/YEkYfwNlT0cDQFttfXt3c39rbA==/?ll=40.945624%2C57.762388&utm_medium=mapframe&utm_source=maps&z=17.1"
+                        style={{color: "#eee", fontSize: "12px", position: "absolute", top: "14px"}}>Советская улица,
+                        44А —
+                        Яндекс
+                        Карты</a>
+                    <iframe title={"ya_maps"} className="adaptive__maps"
+                            src="https://yandex.ru/map-widget/v1/?ll=40.945624%2C57.762388&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NjM0ODY2NRJD0KDQvtGB0YHQuNGPLCDQmtC-0YHRgtGA0L7QvNCwLCDQodC-0LLQtdGC0YHQutCw0Y8g0YPQu9C40YbQsCwgNDTQkCIKDVHII0IVrwxnQg%2C%2C&z=17.1"
+                            width="700" height="476" allowFullScreen={true}
+                            style={{position: "relative"}}></iframe>
                 </div>
 
 
