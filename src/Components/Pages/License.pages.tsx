16115fee63d2028ee52b6cf9acf574767487ee4c
ArtemBelykh <stell_artem@mail.ru>
2025-02-12 00:07:27 +0300

    add doc licens

--- src/Components/Pages/License.pages.tsx
+++ src/Components/Pages/License.pages.tsx
@@ -7,7 +7,8 @@ import ModalAssets from "../ModalAssets";
 
 const LicensePages = () => {
     return (
-        <Container className="fistScreenContainer__adaptive " sx={{marginTop: "120px", height: "100%", marginBottom: "50px"}}>
+        <Container className="fistScreenContainer__adaptive "
+                   sx={{marginTop: "120px", height: "100%", marginBottom: "50px"}}>
 
             <Typography variant="h2" component="h2" sx={{
                 textAlign: "center",
@@ -34,11 +35,13 @@ const LicensePages = () => {
             </Typography>
 
 
-            <Box className="adaptLicence" sx={{display: "flex", justifyContent: "space-between", marginTop: "30px", flexWrap: "wrap" }}>
-                <ModalAssets img="/license_doc/ExtractFromTheRegisterOfLicensesNo_02_21_001.jpg" img2="/license_doc/ExtractFromTheRegisterOfLicensesNo_02_21_002.jpg" isPdf={true}/>
+            <Box className="adaptLicence"
+                 sx={{display: "flex", justifyContent: "space-between", marginTop: "30px", flexWrap: "wrap"}}>
+                <ModalAssets img="/license_doc/ExtractFromTheRegisterOfLicensesNo_02_21_001.jpg"
+                             img2="/license_doc/ExtractFromTheRegisterOfLicensesNo_02_21_002.jpg" isPdf={true}/>
                 <ModalAssets img="/license_doc/2.jpg"/>
                 <ModalAssets img="/license_doc/3.jpg"/>
-                
+
                 <ModalAssets img="/license_doc/docSchool1.jpg"/>
                 <ModalAssets img="/license_doc/docSchool2.jpg"/>
                 <ModalAssets img="/license_doc/docSchool3.jpg"/>
@@ -49,11 +52,22 @@ const LicensePages = () => {
             <br/><br/>
 
 
-            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank" href="/license_doc/act_samoobsledovanya.pdf">Отчет о результатах самообследования 2024 года г. Буй</a><br/>
-            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank" href="/Отчет_о_результатах_самообследования_г_Кострома_2023.pdf">Отчет о результатах самообследования 2023 года г. Кострома</a><br/>
-            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank" href="/Отчет_о_результатах_самообследования_г_Галич_2023.pdf">Отчет о результатах самообследования 2023 года г. Галич</a><br/>
-            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank" href="/Отчет_о_результатах_самообследования_г_Кострома_2021.pdf">Отчет о результатах самообследования 2021 года г. Кострома</a><br/>
-            
+            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank"
+               href="/license_doc/act_samoobsl_2025.pdf">Отчет о результатах самообследования 2025 года г.
+                Ярославль</a><br/>
+            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank"
+               href="/license_doc/act_samoobsledovanya.pdf">Отчет о результатах самообследования 2024 года г.
+                Буй</a><br/>
+            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank"
+               href="/Отчет_о_результатах_самообследования_г_Кострома_2023.pdf">Отчет о результатах самообследования
+                2023 года г. Кострома</a><br/>
+            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank"
+               href="/Отчет_о_результатах_самообследования_г_Галич_2023.pdf">Отчет о результатах самообследования 2023
+                года г. Галич</a><br/>
+            <a style={{color: "blue", textDecoration: "none", fontSize: "18px"}} target="_blank"
+               href="/Отчет_о_результатах_самообследования_г_Кострома_2021.pdf">Отчет о результатах самообследования
+                2021 года г. Кострома</a><br/>
+
         </Container>
     );
 };
